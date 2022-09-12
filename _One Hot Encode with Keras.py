8228b28b35f31921607e77ae92636aefa512b1fb
PAVI2610 <108166911+PAVI2610@users.noreply.github.com>
2022-09-12 23:24:34 +0530

    Add files via upload

---
+++ _One Hot Encode with Keras.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+from numpy import array
+from numpy import argmax
+from keras.utils import to_categorical
+# define example
+data = [1, 3, 2, 0, 3, 2, 2, 1, 0, 1]
+data = array(data)
+print(data)
+# one hot encode
+encoded = to_categorical(data)
+print(encoded)
+# invert encoding
+inverted = argmax(encoded[0])
+print(inverted)
+
+
+# In[ ]:
+
+
+
+
